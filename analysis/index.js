94a7750ae967925eff399fe42bee4fe63848d7d5
edwardpmorris <edward.morris@vizzuality.com>
2020-07-31 11:43:09 +0200

    hello world example

--- analysis/index.js
+++ analysis/index.js
@@ -1,62 +1,24 @@
+// Import earthengine package
 const ee = require('@google/earthengine');
+
+// Define private key for service account
+// see https://developers.google.com/earth-engine/service_account
 const PRIVATE_KEY = require('./credentials.json');
 
-const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+// Define your earthengine function
+var app = (name) => {
+  return "Hello" + name;
+}
 
-const serialize = (originalData) => {
-  if (!originalData || !originalData.length) return null;
-
-  const props = originalData[0].properties;
-  const data = props.histogram;
-  const bucketWidth = data.bucketWidth;
-  const countSum = arrSum(data.histogram);
-
-  return {
-    rows: data.histogram.map((d, i) => ({
-      min: data.bucketMin + (bucketWidth * i),
-      max: data.bucketMin + (bucketWidth * (i + 1)),
-      count: d,
-      percent: d / countSum
-    })),
-    fields: {
-      min: { type: 'number' },
-      max: { type: 'number' },
-      count: { type: 'number' },
-      percent: { type: 'number' }
-    },
-    total_rows: data.histogram.length,
-    stats: {
-       min: props.min,
-      max: props.max,
-      mean: props.mean,
-      stdev: props.stdDev,
-      sum: props.sum
-    }
-  };
-};
-
-const calcHistogram = (assetId, geometry) => {
-  const image = ee.Image(assetId);
-  const reducers = ee.Reducer.histogram(20)
-      .combine(ee.Reducer.minMax(), '', true)
-      .combine(ee.Reducer.mean(),'', true )
-      .combine(ee.Reducer.stdDev(), '', true)
-      .combine(ee.Reducer.sum(), '', true);
-  const regReducer = {
-    collection: ee.FeatureCollection(geometry.features),
-    reducer: reducers
-  };
-  const histogram = image.reduceRegions(regReducer).toList(10000);
-
-  return histogram;
-};
-
+// REST analysis wrapper
 exports.analyse = (req, res) => {
-  const assetId = req.body.assetId;
-  const geometry = req.body.geometry;
 
+  // Define parameters for the function
+  // passed via GET (query)
+  var name = req.query.name;
+
+  // Set permissions
   res.set('Access-Control-Allow-Origin', '*');
-
   if (req.method === 'OPTIONS') {
     // Send response to OPTIONS requests
     res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
@@ -65,17 +27,20 @@ exports.analyse = (req, res) => {
     res.status(204).send('');
   }
 
-  if (!assetId || !geometry) {
+  // Error if required parameter(s) not given
+  if (!name) {
     return res.json({
-      error: 'assetId and geometry are required'
+      error: 'name is required'
     });
   }
 
+  // Authenticate, initiate ee, do analysis, and return the result
   ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
-    ee.initialize(null, null, () => {
-      const result = calcHistogram(assetId, geometry);
+    ee.initialize(null, null, function () {
+      var result = app(name);
       result.evaluate((json) => res.status(200).json(json));
+    }, function (e) {
+      return res.json({ error: 'Initialization error: ' + e });
     });
   });
 };
-
