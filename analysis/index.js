be1b8b51f45f7e56fdefbc1c30aa47d7363f43fd
tamaramegan <tamara.huete@gmail.com>
2020-08-05 14:12:44 +0200

    reverted to histogram

--- analysis/index.js
+++ analysis/index.js
@@ -35,46 +35,26 @@ const serialize = (originalData) => {
   };
 };
 
-// const calcHistogram = (assetId, geometry) => {
-//   const image = ee.Image(assetId);
-//   const reducers = ee.Reducer.histogram(20)
-//       .combine(ee.Reducer.minMax(), '', true)
-//       .combine(ee.Reducer.mean(),'', true )
-//       .combine(ee.Reducer.stdDev(), '', true)
-//       .combine(ee.Reducer.sum(), '', true);
-//   const regReducer = {
-//     collection: ee.FeatureCollection(geometry.features),
-//     reducer: reducers
-//   };
-//   const histogram = image.reduceRegions(regReducer).toList(10000);
+const calcHistogram = (assetId, geometry) => {
+  const image = ee.Image(assetId);
+  const reducers = ee.Reducer.histogram(20)
+      .combine(ee.Reducer.minMax(), '', true)
+      .combine(ee.Reducer.mean(),'', true )
+      .combine(ee.Reducer.stdDev(), '', true)
+      .combine(ee.Reducer.sum(), '', true);
+  const regReducer = {
+    collection: ee.FeatureCollection(geometry.features),
+    reducer: reducers
+  };
+  const histogram = image.reduceRegions(regReducer).toList(10000);
 
-//   return histogram;
-// };
-
-
-// Define your earthengine function
-var app = (name) => {
-  return "Hello" + name;
-}
+  return histogram;
+};
 
 
 exports.analyse = (req, res) => {
   const assetId = req.body.assetId;
   const geometry = req.body.geometry;
-  const name = req.query.name;
-  const test = req.query.this;
-
-  // Error if required parameter(s) not given
-
-  // req.body ==> json payload in POST request
-  // if payload contains the key 'geometry' ==> req.body.geometry
-
-  // req.query ==> localhost:8080?analysis=mean&period=[2015, 2019]
-  // anything after the ? gets cast to  an object
-  // key=value e.g. req.query.analysis ==> 'mean'
-  // separated by &
-  // always return a string: period=[2015, 2019] ==> req.query.period = '[2015, 2019]'
-
 
   res.set('Access-Control-Allow-Origin', '*');
 
@@ -86,20 +66,15 @@ exports.analyse = (req, res) => {
     res.status(204).send('');
   }
 
-  // if (!assetId || !geometry) {
-  //   return res.json({
-  //     error: 'assetId and geometry are required'
-  //   });
-  // // Error if required parameter(s) not given
-  // if (test || name) {
-  //   return res.json({
-  //     error: `name is ${name} and this is ${test} and asset is ${assetId}`
-  //   });
-  // }
+  if (!assetId || !geometry) {
+    return res.json({
+      error: 'assetId and geometry are required'
+    });
+
 
   ee.data.authenticateViaPrivateKey(PRIVATE_KEY, () => {
     ee.initialize(null, null, () => {
-      // const result = calcHistogram(assetId, geometry);
+      const result = calcHistogram(assetId, geometry);
       var result = app(name);
       result.evaluate((json) => res.status(200).json(json));
     });
@@ -107,4 +82,3 @@ exports.analyse = (req, res) => {
 
 
 };
-
