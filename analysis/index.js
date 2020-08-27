9c024f603afb15f941e1a37c6456906749432cdb
edwardpmorris <edward.morris@vizzuality.com>
2020-08-27 09:47:42 +0200

    Added generic REST string parsing

--- analysis/index.js
+++ analysis/index.js
@@ -16,7 +16,6 @@ console.log('info: ee package and Private key loaded');
 const hfs = require('./helper_functions.js')
 console.log('info: helper functions loaded');
 
-
 //////////////////////////////////////////////////////////////////////////////
 // REST WRAPPER
 //////////////////////////////////////////////////////////////////////////////
@@ -26,7 +25,7 @@ exports.analyse = (req, res) => {
   //////////////////////////////////////////////////////////////////////////////
   // CONFIG
   //////////////////////////////////////////////////////////////////////////////
- 
+
   // Set response permissions
   res.set('Access-Control-Allow-Origin', '*');
 
@@ -38,9 +37,31 @@ exports.analyse = (req, res) => {
     res.set('Access-Control-Max-Age', '3600');
     res.status(204).send('');
   }
-  
+
   //////////////////////////////////////////////////////////////////////////////
-  // PARMAMETERS
+  // PARSERS
+  //////////////////////////////////////////////////////////////////////////////
+
+  function type(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1)
+  }
+
+  function parseRequest(s, name, type, required, def_value) {
+    var out = s
+    if (s === undefined && required === true) {
+      out = name + ' is required}'
+      res.json({ error: out })
+    }
+    if (s === undefined && required === false) { out = def_value }
+    if (type === 'String'){out = '{"value": "' + out + '"}'}
+    if (type === 'Array'){out = '{"value": ' + out + '}'}
+    if (type === 'Number'){out = '{"value": ' + out + '}'}
+    if (type === 'Boolean'){out = '{"value": ' + out + '}'}
+    return JSON.parse(out).value
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // PARAMETERS
   //////////////////////////////////////////////////////////////////////////////
 
   // Define parameters used in the analysis
@@ -50,75 +71,51 @@ exports.analyse = (req, res) => {
 
   // @param {string} fid Feature ID  
   // default: REQUIRED
-  // parse from request
-  var fid = req.query.fid;
-  // set default or error
-  if (fid === undefined) { res.json({error: 'fid is required, try fid=1_2_22'}); }
-  // print to console
+  var fid = parseRequest(req.query.fid, 'fid', 'String', true, null)
   console.log('info: fid=' + fid);
-  console.log(typeof fid);
+  console.log(type(fid));
 
   // @param {list of strings} analysis_types The keywords for the types of analysis to return 
   // default: ['land-cover', 'mangrove-properties', 'mangrove-carbon', 'length-coast']
-  // parse request
-  var analysis_types = req.query.analysis_types
-  // set default or error
-  if (analysis_types === undefined) { 
-    analysis_types = '["land-cover","mangrove-properties", "mangrove-carbon"]'; }
-  analysis_types = JSON.parse('{ "at":' + analysis_types +  ' }').at;
-    // print to console
+  var analysis_types = parseRequest(req.query.analysis_types, 'analysis_types', 'Array', false, '["land-cover","mangrove-properties", "mangrove-carbon"]');
   console.log('info: analysis_types=' + analysis_types);
-  console.log(typeof analysis_types);
+  console.log(type(analysis_types));
 
   // @param {list of strings} timestamps List of ISO8601 timestamp strings 
   // default: ['2016-01-01T00:00:00']
-  // parse request
-  var timestamps = req.query.timestamps
-  // set default or error
-  if (timestamps === undefined) { timestamps = '["2016-01-01T00:00:00"]'; }
-  timestamps = JSON.parse('{ "ts":' + timestamps + ' }').ts
+  var timestamps = parseRequest(req.query.timestamps, 'timestamps', 'Array', false, '["2016-01-01T00:00:00"]');
   console.log('info: timestamps=' + timestamps);
-  console.log(typeof timestamps);
+  console.log(type(timestamps));
 
   // @param {number} buffer distance The distance in meters to buffer pixels of interest when using vector line intersection
   // default: 200
-  // parse request
-  var buffer_distance_m = req.query.buffer_distance_m;
-  // set default or error
-  if (buffer_distance_m === undefined) { buffer_distance_m = 200; }
-  buffer_distance = parseInt(buffer_distance_m)
+  var buffer_distance_m = parseRequest(req.query.buffer_distance_m, 'buffer_distance_m', 'Number', false, 200);
   console.log('info: buffer_distance_m=' + buffer_distance_m);
-  console.log(typeof buffer_distance_m);
+  console.log(type(buffer_distance_m));
 
   // @param {number} scale The scale in meters for reducer calculations and simplifying of vectors
   // default: 30
-  // parse request
-  var scale = parseInt(req.query.scale);
-  // set default or error
-  if (scale === undefined) { scale = 30; }
-  scale = parseInt(scale);
+  var scale = parseRequest(req.query.scale, 'scale', 'Number', false, 30)
   console.log('info: scale=' + scale);
-  console.log(typeof scale);
+  console.log(type(scale));
 
   // @param {boolean} bestEffort Use bestEffort in reducer calculations;
   // this can allow faster calculations for larger geometries, but at an undefined scale 
   // default: false
-  // parse request set default or error
-  var bestEffort = false
-  bestEffort = (req.query.bestEffort === 'true');
+  var bestEffort = parseRequest(req.query.bestEffort, 'bestEffort', 'Boolean', false, false)
   console.log('info: bestEffort=' + bestEffort);
-  console.log(typeof bestEffort);
-  
+  console.log(type(bestEffort));
+
   //////////////////////////////////////////////////////////////////////////////
   // ANALYSIS
   //////////////////////////////////////////////////////////////////////////////
-  
+
   // FIXME: correctly return errors
 
   // Authenticate using a service account.
   ee.data.authenticateViaPrivateKey(privateKey, function (e) {
     console.log('info: Authenticating earth-engine')
-    
+
     // Initialise earth engine
     ee.initialize(null, null, function () {
       console.log('info: Initializing earth-engine')
@@ -134,26 +131,26 @@ exports.analyse = (req, res) => {
       // CONVERT PARAMS TO EE OBJECTS
       // this may not be needed, but is usually good practice:
       // "ee server functions should always take and return ee objects"
-      
+
       // Convert fid from string to ee.String
       fid = ee.String(fid);
-      //fid.evaluate(function(info) {console.log(info);})
-      
+      fid.evaluate(function(info) {console.log(info);})
+
       // Convert timestamps from list of strings to ee.List of ee.Date
       timestamps = ee.List(timestamps).map(function (s) { return ee.Date(s) });
-      //timestamps.evaluate(function(info) {console.log(info);})
-      
+      timestamps.evaluate(function(info) {console.log(info);})
+
       // Convert buffer_distance_m from number to ee.Number 
       buffer_distance_m = ee.Number(buffer_distance_m);
-      //buffer_distance_m.evaluate(function(info) {console.log(info);})
+      buffer_distance_m.evaluate(function(info) {console.log(info);})
 
       // Convert buffer_distance_m from number to ee.Number 
       scale = ee.Number(scale);
-      //buffer_distance_m.evaluate(function(info) {console.log(info);})
-      
+      scale.evaluate(function(info) {console.log(info);})
+
       // Convert analysis_types from list of strings to ee.List of ee.String
       analysis_types = ee.List(analysis_types);
-      //analysis_types.evaluate(function(info) {console.log(info);})  
+      analysis_types.evaluate(function(info) {console.log(info);})  
 
       //////////////////////////////////////////////////////////////////////////////
       // GET SINGLE FEATURE
@@ -185,7 +182,7 @@ exports.analyse = (req, res) => {
       // RETURN RESULT AS JSON TO CLIENT
       console.log('info: Returning results as a GeoJSON Feature')
       out.evaluate((json) => res.status(200).json(json))
-    
+
     }); // End initialise earth engine
   }); // End authenticate using a service account.
 
